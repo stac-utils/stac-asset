5fda24b9c8ab87faf4a153a67c105d626fc14c79
Pete Gadomski <pete.gadomski@gmail.com>
2024-06-18 13:32:59 -0700

    Update src/stac_asset/config.py

--- src/stac_asset/config.py
+++ src/stac_asset/config.py
@@ -85,7 +85,7 @@ class Config:
     """The maximum number of attempts when downloading assets from s3."""
 
     s3_endpoint_url: Optional[str] = None
-    """Set an optional custom endpoint url for s3"""
+    """Set an optional custom endpoint url for s3."""
 
     def validate(self) -> None:
         """Validates this configuration.
