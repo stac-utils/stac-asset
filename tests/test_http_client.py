287c6e177943a7910478bf7c7713dcd2d880c047
Stijn Caerts <stijn.caerts@vito.be>
2025-06-18 13:45:20 +0200

    feat: support OAuth2 via HTTP client middleware instead of subclass

--- tests/test_http_client.py
+++ tests/test_http_client.py
@@ -1,11 +1,11 @@
 import pytest
-from aiohttp_oauth2_client.client import OAuth2Client
 from aiohttp_oauth2_client.grant.authorization_code import AuthorizationCodeGrant
 from aiohttp_oauth2_client.grant.client_credentials import ClientCredentialsGrant
 from aiohttp_oauth2_client.grant.device_code import DeviceCodeGrant
 from aiohttp_oauth2_client.grant.resource_owner_password_credentials import (
     ResourceOwnerPasswordCredentialsGrant,
 )
+from aiohttp_oauth2_client.middleware import OAuth2Middleware
 
 from stac_asset import Config, HttpClient
 
@@ -33,8 +33,9 @@ async def test_oauth2_device_code_config() -> None:
         oauth2_client_id="public",
     )
     async with await HttpClient.from_config(config) as client:
-        assert isinstance(client.session, OAuth2Client)
-        assert isinstance(client.session.grant, DeviceCodeGrant)
+        middlewares = client.session._client._middlewares
+        assert isinstance(middlewares[0], OAuth2Middleware)
+        assert isinstance(middlewares[0].grant, DeviceCodeGrant)
 
 
 async def test_oauth2_authorization_code_config() -> None:
@@ -46,8 +47,9 @@ async def test_oauth2_authorization_code_config() -> None:
         oauth2_pkce=False,
     )
     async with await HttpClient.from_config(config) as client:
-        assert isinstance(client.session, OAuth2Client)
-        assert isinstance(client.session.grant, AuthorizationCodeGrant)
+        middlewares = client.session._client._middlewares
+        assert isinstance(middlewares[0], OAuth2Middleware)
+        assert isinstance(middlewares[0].grant, AuthorizationCodeGrant)
 
 
 async def test_oauth2_password_config() -> None:
@@ -59,8 +61,9 @@ async def test_oauth2_password_config() -> None:
         oauth2_password="secret",
     )
     async with await HttpClient.from_config(config) as client:
-        assert isinstance(client.session, OAuth2Client)
-        assert isinstance(client.session.grant, ResourceOwnerPasswordCredentialsGrant)
+        middlewares = client.session._client._middlewares
+        assert isinstance(middlewares[0], OAuth2Middleware)
+        assert isinstance(middlewares[0].grant, ResourceOwnerPasswordCredentialsGrant)
 
 
 async def test_oauth2_client_credentials_config() -> None:
@@ -71,5 +74,6 @@ async def test_oauth2_client_credentials_config() -> None:
         oauth2_client_secret="secret",
     )
     async with await HttpClient.from_config(config) as client:
-        assert isinstance(client.session, OAuth2Client)
-        assert isinstance(client.session.grant, ClientCredentialsGrant)
+        middlewares = client.session._client._middlewares
+        assert isinstance(middlewares[0], OAuth2Middleware)
+        assert isinstance(middlewares[0].grant, ClientCredentialsGrant)
